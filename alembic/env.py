f9fc31b288a46ba499ce2b518d2fda70194d6791
kanishkaa255 <102449679+kanishkaa255@users.noreply.github.com>
2026-08-11 15:41:03 -0500

    add docker-compose.test.yml and environment-based DB URL override for CI

--- alembic/env.py
+++ alembic/env.py
@@ -14,6 +14,12 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+import os
+
+url = os.environ.get("DATABASE_URL")
+if url:
+    config.set_main_option("sqlalchemy.url", url)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
