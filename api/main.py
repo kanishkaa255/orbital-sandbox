b8926c18e247f887397303188bd7edf812dc209b
kanishkaa255 <102449679+kanishkaa255@users.noreply.github.com>
2026-08-16 22:43:55 -0500

    fix ruff issue

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+import os
+
 import redis
 from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
@@ -10,7 +12,6 @@ from .db import SessionLocal
 from .models import Simulation
 from .schemas import SimulationCreate, SimulationOut
 
-import os
 redis_conn = redis.from_url(os.environ.get("REDIS_URL", "redis://localhost:6379"))
 queue = Queue(connection=redis_conn)
 
